49714f4fab8f20883dfffe914a8f66eb6fe90ea0
AirRio <45617410+AirRio@users.noreply.github.com>
2021-05-31 01:46:02 +0300

    Update StringParser.kt

--- src/main/kotlin/subtask3/StringParser.kt
+++ src/main/kotlin/subtask3/StringParser.kt
@@ -4,6 +4,27 @@ class StringParser {
 
     // TODO: Complete the following function
     fun getResult(inputString: String): Array<String> {
-        throw NotImplementedError("Not implemented")
+
+        var result = arrayListOf<String>()
+        var count = 0
+
+        for(char in inputString.withIndex())
+            if(char.value in charArrayOf('<', '[', '('))
+            {
+                val index = charArrayOf('<', '[', '(').indexOf(char.value)
+
+                for(text in char.index + 1 until inputString.length)
+                {
+                     if(inputString[text] == char.value)  count++
+                     if(inputString[text] == charArrayOf('>', ']', ')')[index])
+                            if (count == 0) {
+                                result.add(inputString.substring(char.index + 1, text))
+                            } else {
+                                count--
+                            }
+                }
+            }
+
+        return result.toTypedArray()
     }
 }
